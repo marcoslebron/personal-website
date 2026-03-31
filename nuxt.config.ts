5ea084f999aaaeb6b41a909be6cd428f6937dfae
Marcos <lebron.marcos@gmail.com>
2026-03-31 18:15:10 -0400

    config netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
-    '@nuxt/test-utils/module'
+    '@nuxt/test-utils/module',
+    '@netlify/nuxt',
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
